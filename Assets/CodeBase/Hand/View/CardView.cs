22bbd504bab5c4b51ac4c37840c6e26ad0f9b8bc
OtakakOtaa <OtakakOtaaa@gmail.ru>
2023-02-13 17:46:14 +0300

    init GlowFrameShader and add Behevior

--- Assets/CodeBase/Hand/View/CardView.cs
+++ Assets/CodeBase/Hand/View/CardView.cs
@@ -12,7 +12,7 @@ namespace CodeBase.Hand.View
         [SerializeField] private TextMeshProUGUI _healthState;
         [SerializeField] private TextMeshProUGUI _manaCostState;
         [SerializeField] private TextMeshProUGUI _attackState;
-        
+
         [SerializeField] private TextMeshProUGUI _headerText;
         [SerializeField] private TextMeshProUGUI _descriptionText;
 
@@ -21,8 +21,12 @@ namespace CodeBase.Hand.View
         [SerializeField] private SpriteRenderer _header;
         [SerializeField] private Canvas _canvas;
 
+        [SerializeField] private Material _glowFrameMaterial;
+        private GlowFrameShader _glowFrameShader;
+
+
         public event Action InitializeEnded;
-        
+
         public int LayerComponentsCount => 4;
 
         public GrippeableObject GrippeableObject { get; private set; }
@@ -30,9 +34,22 @@ namespace CodeBase.Hand.View
         public void Start()
         {
             _canvas.worldCamera = Camera.current;
+
             GrippeableObject = GetComponent<GrippeableObject>();
+            
+            _glowFrameShader = new GlowFrameShader();
+            DuplicateMaterial();
+            GrippeableObject.StartCaptured += EnableFrameGlow;
+            GrippeableObject.StopCaptured += DisableFrameGlow;
+
             InitializeEnded?.Invoke();
         }
+
+        private void EnableFrameGlow()
+            => _glowFrameMaterial.SetFloat(_glowFrameShader.PowerValueId, _glowFrameShader.OnEnableGlowAmount);
+
+        private void DisableFrameGlow()
+            => _glowFrameMaterial.SetFloat(_glowFrameShader.PowerValueId, _glowFrameShader.OnDisableGlowAmount);
 
         public void UpdateParam(Card card)
         {
@@ -43,7 +60,7 @@ namespace CodeBase.Hand.View
 
         public void SetImage(Sprite image)
             => _image.sprite = image;
-        
+
         public void SetLayerOrder(int orderAmount)
         {
             _image.sortingOrder = orderAmount++;
@@ -51,9 +68,22 @@ namespace CodeBase.Hand.View
             _header.sortingOrder = orderAmount++;
             _canvas.sortingOrder = orderAmount;
         }
+
+        private void DuplicateMaterial()
+        {
+            _glowFrameMaterial = new Material(_glowFrameMaterial);
+            _frame.material = _glowFrameMaterial;
+        }
         
         private const string HealthStateHeader = "Health : ";
         private const string ManaCostStateHeader = "Manacost : ";
         private const string AttackStateHeader = "Attack : ";
+
+        class GlowFrameShader
+        {
+            public int PowerValueId => Shader.PropertyToID("_Flutness");
+            public float OnEnableGlowAmount => 0.009f;
+            public float OnDisableGlowAmount => default;
+        }
     }
 }
